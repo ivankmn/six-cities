75c38c01faa7f863a82e25a5191d80e2b6846c24
Ivan Klimukhin <ivankmn@yandex.ru>
2025-08-04 18:01:09 +0300

    добавил в api получение отзывов и мест неподалеку

--- project/src/api/api.ts
+++ project/src/api/api.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Offer } from '../types/offer';
+import { ReviewItem } from '../types/review';
 
 const BACKEND_URL = 'https://12.react.htmlacademy.pro/six-cities/';
 
@@ -9,6 +10,12 @@ export const api = createApi({
   endpoints: (builder) => ({
     getLocations: builder.query<Offer[], void>({
       query: () => 'hotels',
+    }),
+    getReviews: builder.query<ReviewItem[], void>({
+      query: () => 'reviews',
+    }),
+    getLocationsNearby: builder.query<Offer[], string>({
+      query: (hotelId) => `${hotelId}/nearby`,
     }),
   }),
 });
